1857ddead6c3f295b7a50d2bf68f6c1a91b26534
Maxim Egorov <maxgegorov@gmail.com>
2015-08-26 11:45:45 -0700

    Fixed a type bug

--- src/RolloutPolicies.jl
+++ src/RolloutPolicies.jl
@@ -5,6 +5,7 @@ using DiscreteSniper
 
 export
     RandomPolicy,
+    SniperHeurisitcPolicy,
     action
 
 import POMDPs: action
@@ -12,6 +13,16 @@ import POMDPs: action
 
 type RandomPolicy <: Policy
     pomdp::SniperPOMDP
+end
+
+# local search for point maximizing the metric: wt * dt + wr * dr
+type SniperHeurisitcPolicy <: Policy
+    pomdp::SniperPOMDP
+    wt::Float64 # target weight
+    wr::Float64 # resource weight
+end
+function SniperHeurisitcPolicy(pomdp::SniperPOMDP)
+    return SniperHeurisitcPolicy(pomdp, 0.1, 1.0)
 end
 
 # returns a valid random action
@@ -28,5 +39,13 @@ function action(policy::RandomPolicy, s::Int64)
     return a
 end
 
+
+function action(policy::SniperHeurisitcPolicy, s::Int64)
+    # find point with highest score
+    # brute force enumerate
+    
+    # find unit vector direction
+    # pick action that goes in that direction
+end
 
 end # module
