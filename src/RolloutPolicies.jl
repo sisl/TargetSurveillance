4414a5275e563ed84eeb042177b4d6f8a8067a7d
Maxim Egorov <maxgegorov@gmail.com>
2015-08-27 11:13:06 -0700

    Added belief based random rollout

--- src/RolloutPolicies.jl
+++ src/RolloutPolicies.jl
@@ -39,6 +39,19 @@ function action(policy::RandomPolicy, s::Int64)
     return a
 end
 
+# returns a valid random action
+function action(policy::RandomPolicy, s::Int64, b::Belief)
+    pomdp = policy.pomdp
+    xy = pomdp.temp_position
+    n = n_actions(pomdp)
+    i2xy!(xy, pomdp, s)
+    p = xy[1]
+    a = rand(1:n)
+    while !valid_action(pomdp, p, a)
+        a = rand(1:n)
+    end
+    return a
+end
 
 function action(policy::SniperHeurisitcPolicy, s::Int64)
     # find point with highest score
